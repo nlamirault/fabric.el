780cb7c547aae15ddae130e6bfe6d1835e4efdc3
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-02-03 09:21:03 +0100

    FIX tests configuration

--- test/ert-loader.el
+++ test/ert-loader.el
@@ -1,14 +1,18 @@
 ;;; ert-loader.el --- Load Ert if not included in Emacs
 
 (require 'f)
+(eval-when-compile
+  (require 'cl))
 
-(defvar commander-root-path
+
+(defvar fabric-root-path
   (f-parent (f-parent load-file-name))
   "Path to root.")
 
-(defvar commander-vendor-path
+(defvar fabric-vendor-path
   (f-expand "vendor" commander-root-path)
   "Path to vendor.")
 
 (unless (require 'ert nil 'noerror)
-  (require 'ert (f-expand "ert" commander-vendor-path)))
+  (require 'ert (f-expand "ert" fabric-vendor-path)))
+
---
+++ vagrant/provision.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Title           : Vagrant provision
+# Description     : Install Emacs Snapshot
+# ------------------------------------------------------
+
+ppa () {
+    sudo apt-add-repository -y "$1"
+}
+
+apt_update () {
+    sudo apt-get update -qq
+}
+
+apt () {
+    sudo apt-get install -yy "$@"
+}
+
+# Silence debconf
+export DEBIAN_FRONTEND='noninteractive'
+
+# Bring in the necessary PPAs
+ppa ppa:cassou/emacs
+apt_update
+
+# Install Emacs 24.x and Emacs snapshot
+apt emacs24 emacs24-el emacs24-common-non-dfsg \
+    emacs-snapshot emacs-snapshot-el
+
+# Install Cask for Emacs dependency management
+CASK_DIR=/opt/cask-0.5.0
+if ! [ -d "$CASK_DIR" -a -x "/$CASK_DIR/bin/cask" ]; then
+  sudo rm -rf "$CASK_DIR"
+  wget -O - https://github.com/cask/cask/releases/v0.5.1.tar.gz | sudo tar xz -C /opt
+  # Bring Cask into $PATH
+  sudo ln -fs "$CASK_DIR/bin/cask" /usr/local/bin
+fi
