48090349b5197c0f02b319b2d1281ac073e93805
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-02-04 12:14:41 +0100

    Add license header

--- test/fabric-version-test.el
+++ test/fabric-version-test.el
@@ -7,6 +7,23 @@
 ;; Commentary:
 ;; Unit tests for version information
 
+;;; License:
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Code:
 
 
@@ -23,5 +40,8 @@
     ;;(should (string= version (fabric-mode-library-version)))))
     (should (string= "0.1.0" cask-version))))
 
+
+
+(provide 'fabric-version-test)
 
 ;;; version-test.el ends here
